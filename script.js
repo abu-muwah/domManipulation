afd87ee9ef87a55d0477f3afb5ccf6066cc1110d
abu-muwah <abu.muwah@gmail.com>
2022-08-22 15:36:57 +0430

    Add functionality

--- script.js
+++ script.js
@@ -18,6 +18,57 @@
 //   console.log(document.querySelector(".input").value);
 // });
 
-document.querySelector(".page-title").textContent = "Guess Your Number";
+// document.querySelector(".page-title").textContent = "Guess Your Number";
 // document.querySelector("a").style.backgroundColor = "#999";
 // document.querySelector("a").style.color = "black";
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+let score = 20;
+let highscore;
+
+const setTextContent = (element, value) => {
+  document.querySelector(element).textContent = value;
+};
+
+document.querySelector("body").style.backgroundColor = "value";
+
+document.querySelector(".add-btn").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".input").value);
+
+  if (!guess) {
+    document.querySelector(".msg").textContent = "No Number :(";
+  } else if (guess === secretNumber) {
+    document.querySelector("body").style.backgroundColor = "green";
+    setTextContent(".number", secretNumber);
+    setTextContent(".msg", "Congrats!! You Did it");
+    // document.querySelector(".input&btn").style.display = none;
+    highscore = score;
+    setTextContent(".highscore", highscore);
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      setTextContent(".msg", "Too High!");
+      score--;
+      setTextContent(".score", score);
+    } else {
+      document.querySelector("body").style.backgroundColor = "red";
+      setTextContent(".msg", "Nahh, get outta here!");
+      setTextContent(".score", 0);
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      setTextContent(".msg", "Too Low!");
+      score--;
+      setTextContent(".score", score);
+    } else {
+      document.querySelector("body").style.backgroundColor = "red";
+      setTextContent(".msg", "Nahh, get outta here!");
+      setTextContent(".score", 0);
+    }
+  }
+});
+
+document.querySelector(".relaod").addEventListener("click", function () {
+  score = 20;
+  setTextContent(".score", score);
+});
