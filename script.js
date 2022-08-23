7c9b8915ebe5ea1258b01e0b5569e8e20f519221
abu-muwah <abu.muwah@gmail.com>
2022-08-23 11:01:15 +0430

    Fixing indentation

--- script.js
+++ script.js
@@ -28,47 +28,47 @@ let score = 20;
 let highscore;
 
 const setTextContent = (element, value) => {
-  document.querySelector(element).textContent = value;
+   document.querySelector(element).textContent = value;
 };
 
 document.querySelector("body").style.backgroundColor = "value";
 
 document.querySelector(".add-btn").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".input").value);
+   const guess = Number(document.querySelector(".input").value);
 
-  if (!guess) {
-    document.querySelector(".msg").textContent = "No Number :(";
-  } else if (guess === secretNumber) {
-    document.querySelector("body").style.backgroundColor = "green";
-    setTextContent(".number", secretNumber);
-    setTextContent(".msg", "Congrats!! You Did it");
-    // document.querySelector(".input&btn").style.display = none;
-    highscore = score;
-    setTextContent(".highscore", highscore);
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      setTextContent(".msg", "Too High!");
-      score--;
-      setTextContent(".score", score);
-    } else {
-      document.querySelector("body").style.backgroundColor = "red";
-      setTextContent(".msg", "Nahh, get outta here!");
-      setTextContent(".score", 0);
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      setTextContent(".msg", "Too Low!");
-      score--;
-      setTextContent(".score", score);
-    } else {
-      document.querySelector("body").style.backgroundColor = "red";
-      setTextContent(".msg", "Nahh, get outta here!");
-      setTextContent(".score", 0);
-    }
-  }
+   if (!guess) {
+      document.querySelector(".msg").textContent = "No Number :(";
+   }else if (guess === secretNumber) {
+      document.querySelector("body").style.backgroundColor = "green";
+      setTextContent(".number", secretNumber);
+      setTextContent(".msg", "Congrats!! You Did it");
+      // document.querySelector(".input&btn").style.display = none;
+      highscore = score;
+      setTextContent(".highscore", highscore);
+   }else if (guess > secretNumber) {
+      if (score > 1) {
+         setTextContent(".msg", "Too High!");
+         score--;
+         setTextContent(".score", score);
+      }else {
+         document.querySelector("body").style.backgroundColor = "red";
+         setTextContent(".msg", "Nahh, get outta here!");
+         setTextContent(".score", 0);
+      }
+   }else if (guess < secretNumber) {
+      if (score > 1) {
+         setTextContent(".msg", "Too Low!");
+         score--;
+         setTextContent(".score", score);
+      }else {
+         document.querySelector("body").style.backgroundColor = "red";
+         setTextContent(".msg", "Nahh, get outta here!");
+         setTextContent(".score", 0);
+      }
+   }
 });
 
 document.querySelector(".relaod").addEventListener("click", function () {
-  score = 20;
-  setTextContent(".score", score);
+   score = 20;
+   setTextContent(".score", score);
 });
